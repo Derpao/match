175991c006e9363ed31f8539f830a522c00effd2
DerpaoProNew <derpaopronew@DerpaoProNews-MacBook-Pro.local>
2025-02-16 02:56:44 +0700

    version 6.0

--- app/games/[id]/loading.tsx
+++ app/games/[id]/loading.tsx
@@ -3,21 +3,15 @@ export default function Loading() {
     <div className="w-full min-h-[400px] flex flex-col items-center justify-center">
       <div className="relative w-20 h-20">
         <div className="absolute top-0 left-0 right-0 bottom-0">
-          <div className="w-20 h-20 border-4 border-blue-200 rounded-full animate-spin border-t-blue-600"/>
+          <div className="w-20 h-20 border-8 border-gray-200 rounded-full animate-spin border-t-blue-600"/>
         </div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <svg 
-            className="w-8 h-8 text-blue-600" 
-            fill="none" 
-            stroke="currentColor" 
+          <svg
+            className="w-12 h-12 text-blue-600"
+            fill="currentColor"
             viewBox="0 0 24 24"
           >
-            <path 
-              strokeLinecap="round" 
-              strokeLinejoin="round" 
-              strokeWidth="2" 
-              d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zm-6 0a6 6 0 00-6 6v7h4v-7a2 2 0 012-2 2 2 0 012 2v7h4v-7a6 6 0 00-6-6z"
-            />
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 3.3l1.35-.95c1.82.56 3.37 1.76 4.38 3.34l-.39 1.34-1.35.46L13 9.2V5.3zm-2 0V9.2L7.01 7.49l-1.35-.46-.39-1.34c1.01-1.57 2.56-2.77 4.38-3.34L11 5.3zm-1.57 5.2L6.4 13l-1.81-.85c-.07-.71-.07-1.44 0-2.15l1.85-.63 3.04 1.13zm-1.95 4.81l.39-1.34 1.35-.46L13 14.8v3.89l-1.35.95c-1.82-.56-3.37-1.76-4.38-3.34zm7.04 0c-1.01 1.57-2.56 2.77-4.38 3.34l-1.35-.95V14.8l2.99-1.5 1.35.46.39 1.34z"/>
           </svg>
         </div>
       </div>
